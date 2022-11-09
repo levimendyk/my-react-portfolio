09229ee710df3b1a077fe30395050f91914a5c21
Levi Mendyk <levimendyk@Levis-MacBook-Air.local>
2022-11-09 16:28:23 -0700

    Added initial components build and updated README.md

---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Project() {
+  return (
+    <div className="projectOne">
+      <p>Project One</p>
+      <p>Project Two</p>
+      <p>Project Three</p>
+      <p>Project Four</p>
+      <p>Project Five</p>
+      <p>Project Six</p>
+    </div>
+  );
+}
