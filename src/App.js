0384533d71afe101a8387b2a5d9f0ee66c74cce4
Levi Mendyk <levimendyk@Levis-MacBook-Air.local>
2022-11-09 17:12:48 -0700

    Added the Navigation component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Project from "./components/Project";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
       <Header />
+      <Navigation />
       <Project />
       <Footer />
     </div>
