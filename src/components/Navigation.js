cce5909e2fc35d59529dbb84a55049abbb02618a
Levi Mendyk <levimendyk@Levis-MacBook-Air.local>
2022-11-09 17:52:28 -0700

    Added some styling to the nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,33 @@
 import React from "react";
+import './styles/Navigation.css'
+// import React, { useState } from "react";
 
 export default function Navigation() {
+
+// let [page, setPage] = useState(home);
   return (
     <nav>
       <ul>
+        <button>
         <li>
           <a href="home">Home</a>
         </li>
+        </button>
+        <button>
         <li>
           <a href="about">About Me</a>
         </li>
+        </button>
+        <button>
         <li>
           <a href="projects">Projects</a>
         </li>
+        </button>
+        <button>
         <li>
           <a href="contact">Contact</a>
         </li>
+        </button>
       </ul>
     </nav>
   );
