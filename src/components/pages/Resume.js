c9cce984d6d5ab8469c1336cb3331382b2ed9e5b
Levi Mendyk <levimendyk@Levis-MacBook-Air.local>
2022-11-18 21:57:10 -0700

    Added resume to Resume Page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,25 @@
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import { AiOutlineDownload } from "react-icons/ai";
+import resume from "../../assets/images/levi-resume.jpg"
+
 export default function Resume() {
   return (
-    <button type="button" class="btn btn-secondary btn-lg btn-block">
-      Download Resume
-    </button>
+  <Container fluid className="resume-section">
+  <Particle />
+  <Row style={{ justifyContent: "center", position: "relative" }}>
+    <Button
+      variant="primary"
+      href="https://docs.google.com/document/d/1LwGf8AjUcaP1DKFypkNbVjuUwcJpjcsX/edit"
+      target="_blank"
+      style={{ maxWidth: "250px" }}
+    >
+      <AiOutlineDownload />
+      &nbsp;Download CV
+    </Button>
+  </Row>
+  <img src={resume} alt="resume" />
+  </ Container>
   );
 }
